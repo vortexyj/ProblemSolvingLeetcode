5b0453a74e2e820b3c8ec8f1dc59969e1a1b1c82
Youssef Jehad <58851426+vortexyj@users.noreply.github.com>
2023-10-04 13:47:18 +0300

    Time: 1 ms (42.79%), Space: 40.7 MB (41.41%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,7 +2,6 @@ class Solution {
     public int lengthOfLastWord(String s) {
         String [] splitted = s.split(" ");
         String LastName = splitted[splitted.length-1];
-        int length = LastName.length();
-        return length;
+        return LastName.length();
     }
 }
